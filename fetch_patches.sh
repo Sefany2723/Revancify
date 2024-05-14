d1ac6e3ee5b3c85d1cff961d7e35c315b3306d92
decipher <84389932+decipher3114@users.noreply.github.com>
2024-05-14 11:30:22 +0530

    fix: nullify test string

--- fetch_patches.sh
+++ fetch_patches.sh
@@ -95,7 +95,7 @@ generatedJson=$(jq --null-input --argjson pkgs "$pkgs" --argjson savedJson "$sav
                                     empty
                                 end
                             else
-                                "hi"
+                                null
                             end
                         )
                 ]
